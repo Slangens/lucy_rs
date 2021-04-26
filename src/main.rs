3bf55f93667b50014bf9b329f18f99ba1976e8d2
Slangens <19713098+Slangens@users.noreply.github.com>
2021-04-26 15:39:45 +0200

    Added channellist.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use serenity::{
     async_trait,
     prelude::*,
     model::prelude::*,
+    model::id::GuildId,
     model::id::ChannelId,
     Client,
     utils::MessageBuilder,
@@ -9,23 +10,27 @@ use serenity::{
 };
 use std::collections::HashMap;
 
-#[async_trait]
-trait Command {
-    async fn predicate(lh: &LucyHandler,ctx: &Context,msg: &Message) -> bool;
-    async fn run(lh: &LucyHandler,ctx: &Context,msg: &Message);
-}
-
 struct LucyHandler{ 
     owner_id:u64, 
     current_channel_id: ChannelId, //ID object to be changed at runtime
+    current_server_id: GuildId,
+    hashmap: std::collections::HashMap<std::string::String, CommandEnum>,
 }
-
 impl LucyHandler { 
-    fn new(ownr:u64,ccid:u64)->Self{ //boilerplate constructor
-        LucyHandler{owner_id:ownr,current_channel_id:ChannelId::from(ccid)}
+    fn new(ownr:u64,ccid:u64, ssid:u64)->Self{ //boilerplate constructor
+
+        let mut command_hash = HashMap::new();
+        command_hash.insert(String::from("!ss"), CommandEnum::Serverlist); //make the hashmap here manually.
+        command_hash.insert(String::from("!cs"), CommandEnum::Channellist);
+        //needs proper lifetime management. dunno how xdd
+        LucyHandler{
+            owner_id:ownr
+            ,current_channel_id:ChannelId::from(ccid)
+            ,current_server_id:GuildId::from(ssid)
+            ,hashmap: command_hash
+        }
     }
 }
-
 #[async_trait]
 impl EventHandler for LucyHandler{
     async fn ready(&self, ctx: Context, ready: Ready) {
@@ -34,29 +39,22 @@ impl EventHandler for LucyHandler{
         if let Err(send_error) = &self.current_channel_id.say(&ctx.http, &bootup_message).await {
             println!("Error sending message: {:?}", send_error);      
         }
-    }//Prints a few messages upon startup.
+
+    }//Prints a few messages upon startup and updates the standard objects.
 
     //Idea: Owner sends DM to Bot, Bot takes input from the DM. Bot acts based on that.
     //Prototype: Send hello to a fixed channel
     async fn message(&self, ctx: Context, msg: Message) {       
         if dm_predicate(&self, &ctx, &msg)==true{
-            //Change to hashmap or something. Can't have a Yandev situation here.
-            /*if Serverlist::predicate(&self, &ctx , &msg).await{
-                Serverlist::run(&self, &ctx, &msg).await;
-            }
-            else {
-                generic_texting_execution(&self, &ctx, &msg).await;
-            }*/
-
-            let command_short  = vec![String::from("!ss")];
-            let command_long = vec![CommandEnum::Serverlist];
-            let command_hash: HashMap<_, _> = command_short.iter().zip(command_long.iter()).collect();
-
-             match command_hash.get(&msg.content){
+            
+             match &self.hashmap.get(&msg.content){
                  Some(cmdname) => {
                     match **cmdname {
                         CommandEnum::Serverlist => {
-                            serverlist_run(&self,&ctx,&msg).await;                           
+                            serverlist(&self,&ctx,&msg).await;                           
+                        },
+                        CommandEnum::Channellist => {
+                            channellist(&self, &ctx, &msg).await;
                         }
                     };
                  }
@@ -65,9 +63,6 @@ impl EventHandler for LucyHandler{
                     generic_texting_execution(&self, &ctx, &msg).await;
                  }
              };
-
-             
-
         }
     }
 
@@ -75,43 +70,61 @@ impl EventHandler for LucyHandler{
 
 enum CommandEnum {
     Serverlist,
+    Channellist,
 }
-
-//Command structs
-struct Serverlist;
-#[async_trait]
-impl Command for Serverlist { //Prints a list of servers upon sending "!ss" in OwnerDM.
-    async fn predicate (lh:&LucyHandler,ctx: &Context,msg: &Message) -> bool  {
-        if msg.content == "!ss" && dm_predicate(lh, ctx, msg) { 
-            return true;
-        }
-        return false;
-    }
-
-    async fn run (_lh:&LucyHandler,ctx: &Context,_msg: &Message) {
-        let x = match serenity::http::client::Http::get_current_user(&ctx.http).await{
-            Ok(curr_usr) => curr_usr,
-            Err(current_user_err) => {println!("Couldn't get current user, Error {:?}", current_user_err); return}
-        };
-    
-        if let Ok(guilds) = x.guilds(&ctx.http).await {
-            for (index, guild) in guilds.into_iter().enumerate() {
-                println!("{}: {}", index, guild.name);
-            }
-        }
-    }
-}
-
-async fn serverlist_run (_lh:&LucyHandler,ctx: &Context,_msg: &Message) {
+/*How to make commands(atm, subject to change): 
+1) Make make command function (async)
+2) Make new CommandEnum variant
+3) To to LucyHandler constructor and manually insert key/value pair
+4) Add usecase to EventHandler::message function -> match there
+*/
+//Commands
+async fn serverlist (_lh:&LucyHandler,ctx: &Context,msg: &Message) {
     let x = match serenity::http::client::Http::get_current_user(&ctx.http).await{
         Ok(curr_usr) => curr_usr,
         Err(current_user_err) => {println!("Couldn't get current user, Error {:?}", current_user_err); return}
     };
 
+    /*if let Ok(guilds) = x.guilds(&ctx.http).await {
+        for (index, guild) in guilds.into_iter().enumerate() {
+            println!("{}: {}, {}", index, guild.name, guild.id);
+            if GuildId::from(guild)==lh.current_server_id {
+                println!("the one");
+            }
+        }
+    }*/
+
+
+    let mut serverdisplay = MessageBuilder::new();
+    serverdisplay.push("```\n");
     if let Ok(guilds) = x.guilds(&ctx.http).await {
         for (index, guild) in guilds.into_iter().enumerate() {
-            println!("{}: {}", index, guild.name);
+            serverdisplay.push("[").push(index).push("] ").push(guild.name.clone()).push("\n");
         }
+    }
+    if let Err(serverlist_speak_err) = msg.channel_id.say(&ctx.http,serverdisplay.push("```").build()).await{
+        println!("Could not send serverlist. Error: {:?}",serverlist_speak_err)
+    }
+}
+async fn channellist (lh:&LucyHandler,ctx: &Context,msg: &Message) {
+    
+    let mut channeldisplay = MessageBuilder::new();
+    channeldisplay.push("```\n");    
+    let channellist = match lh.current_server_id.channels(&ctx).await{
+        Ok(channels) => channels,
+        Err(channelretrieve_err) => {
+            channeldisplay.push(format!("Failed to retrieve channels here, error {:?} \n",channelretrieve_err));
+            return
+        }
+    };
+    let chlist_iter = channellist.values();
+    let mut i:u32=0;
+    for channel in chlist_iter {               
+        channeldisplay.push("[").push(i).push("] ").push(&channel.name).push("\n");
+        i+=1;
+    }
+    if let Err(channellist_speak_err) = msg.channel_id.say(&ctx.http,channeldisplay.push("```").build()).await{
+        println!("Could not send serverlist. Error: {:?}",channellist_speak_err)
     }
 }
 
@@ -140,7 +153,7 @@ fn message_processing(mess: String) -> String{
 
 #[tokio::main]
 async fn main() {
-    let handler = LucyHandler::new(251121149981884423,432969367534305283);
+    let handler = LucyHandler::new(251121149981884423,432969367534305283,432969367534305281);
     let token = std::env::var("DISCORD_TOKEN").expect("No token found.");
     let mut lucy_client = Client::builder(token)
         .event_handler(handler)
@@ -151,3 +164,6 @@ async fn main() {
         println!("Failed to boot Lucy, error: {:?}", boot_error);
     } 
 }
+
+//print server/channellist as embed
+//Take user input 
